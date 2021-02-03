6eb45e3e6186ba10f2d9eb6806dd27df795bec84
anicca <1080Ti>
2021-02-03 21:51:56 +0800

    yya

--- main.py
+++ main.py
@@ -1,11 +1,18 @@
 # -*- coding: utf-8 -*-
 import argparse
+from misc.voc_xml_parser import VocParser
+import torch
 
 parser = argparse.ArgumentParser()
 
+parser.add_argument('--xml_path', type=str, default=r'D:\ppppppppppp\!Zhang-Jia Project\program\label-qr-code', help='Voc xml folder')
+args = parser.parse_args()
+
+
 if __name__ == "__main__":
+    # xml 的 labeling data
+    xml_dlist = VocParser(args.xml_path).get_dlist()
 
-    parser.add_argument('--some_setting', type=str, default='value', help='some desc')
-    args = parser.parse_args()
 
-    
+
+    print("exit")
